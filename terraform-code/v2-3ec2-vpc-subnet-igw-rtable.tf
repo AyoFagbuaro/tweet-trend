bb0a2373920f7c21dd40a687e9524e739ceb9022
Ropo fagbuaro <Ropofagbuaro@yahoo.com>
2025-02-06 00:02:32 +0000

    made changes to terraform file

--- terraform-code/v2-3ec2-vpc-subnet-igw-rtable.tf
+++ terraform-code/v2-3ec2-vpc-subnet-igw-rtable.tf
@@ -7,96 +7,109 @@ resource "aws_instance" "demo-server" {
   ami           = "ami-091f18e98bc129c4e"
   instance_type = "t2.micro"
   key_name = "oyin"
-  vpc_security_group_ids = [aws_security_group.demo_sg.id]
-  subnet_id = aws_subnet.demo_sg-Subnet-01.id
+  vpc_security_group_ids = [aws_security_group.demo-sg.id]
+  subnet_id = aws_subnet.demo-public-Subnet-01.id
+  
+  
   for_each = toset(["Jenkins-master", "Jenkins-slave", "Ansible"])
   tags = {
-    name = "${each.key}"
+    Name = "${each.key}"
 
 }
 }
 
-resource "aws_security_group" "demo_sg" {
-  name        = "demo_sg"
-  description = "SSH access"
-  vpc_id      = aws_vpc.demo_sg-vpc.id
-
+resource "aws_vpc" "demo-vpc"  {
+  cidr_block = "10.1.0.0/16"
   tags = {
-    Name = "ssh-access"
+    Name = "demo-vpc"
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ssh_access_ipv4" {
-  security_group_id = aws_security_group.demo_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  vpc_id      = aws_vpc.demo-vpc.id
+  
+  ingress {
+    description      = "Shh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+    ingress {
+    description      = "Jenkins port"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-
-resource "aws_vpc_security_group_egress_rule" "ssh_access_ipv4" {
-  security_group_id = aws_security_group.demo_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
-
-resource "aws_vpc" "demo_sg-vpc"  {
-  cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "demo_sg-VPC"
+    Name = "ssh-prot"
+
   }
 }
 
 
-resource "aws_subnet" "demo_sg-Subnet-01" {
-  vpc_id     = aws_vpc.demo_sg-vpc.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "demo-public-Subnet-01" {
+  vpc_id     = aws_vpc.demo-vpc.id
+  cidr_block = "10.1.1.0/24"
   map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-2a"
+  availability_zone = "eu-west-2a"
 
   tags = {
-    Name = "demo_sg-Subnet-01"
+    Name = "demo-public-Subnet-01"
   }
 }
 
-resource "aws_subnet" "demo_sg-Subnet-02" {
-  vpc_id     = aws_vpc.demo_sg-vpc.id
-  cidr_block = "10.0.2.0/24"
+resource "aws_subnet" "demo-public-Subnet-02" {
+  vpc_id     = aws_vpc.demo-vpc.id
+  cidr_block = "10.1.2.0/24"
   map_public_ip_on_launch = "true"
   availability_zone = "eu-west-2b"
 
   tags = {
-    Name = "demo_sg-Subnet-02"
+    Name = "demo-public-Subnet-02"
   }
 }
 
-resource "aws_internet_gateway" "demo_sg-igw" {
-  vpc_id = aws_vpc.demo_sg-vpc.id
+
+resource "aws_internet_gateway" "demo-igw" {
+  vpc_id = aws_vpc.demo-vpc.id
 
   tags = {
-    Name = "demo_sg-IGW"
+    Name = "demo-IGW"
   }
 }
 
-resource "aws_route_table" "demo_sg-public-route-table" {
-  vpc_id = aws_vpc.demo_sg-vpc.id
+resource "aws_route_table" "demo-public-route-table" {
+  vpc_id = aws_vpc.demo-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.demo_sg-igw.id
+    gateway_id = aws_internet_gateway.demo-igw.id
+  }
+  tags =  {
+    Name = "demo-public-route-table"
   }
 }
 
-resource "aws_route_table_association" "demo_sg-public-route-table-association-01" {
-  subnet_id      = aws_subnet.demo_sg-Subnet-01.id
-  route_table_id = aws_route_table.demo_sg-public-route-table.id
+resource "aws_route_table_association" "demo-public-route-table-association-01" {
+  subnet_id      = aws_subnet.demo-public-Subnet-01.id
+  route_table_id = aws_route_table.demo-public-route-table.id
 }
 
-resource "aws_route_table_association" "demo_sg-public-route-table-association-02" {
-  subnet_id      = aws_subnet.demo_sg-Subnet-02.id
-  route_table_id = aws_route_table.demo_sg-public-route-table.id
+resource "aws_route_table_association" "demo-public-route-table-association-02" {
+  subnet_id      = aws_subnet.demo-public-Subnet-02.id
+  route_table_id = aws_route_table.demo-public-route-table.id
 }
 
-# 
